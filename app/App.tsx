dd9160968f3daa9748611b5c0f96056b4d519c7f
louisgt3rs <louisxdb@gmail.com>
2026-05-07 19:27:40 +0200

    debug: remove StatusBar+SafeAreaProvider to isolate crash

--- app/App.tsx
+++ app/App.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { RootNavigator } from './src/navigation';
 
 export default function App() {
-  return (
-    <SafeAreaProvider>
-      <StatusBar style="light" />
-      <RootNavigator />
-    </SafeAreaProvider>
-  );
+  return <RootNavigator />;
 }
